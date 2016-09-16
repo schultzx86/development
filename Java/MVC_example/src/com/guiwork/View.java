b78dba12eebc3db6f81a8990d33135f8617673bb
Steven Schultz <Steven@Stevens-iMac.local>
2016-09-16 00:06:57 -0400

    Added rudimentary menu bar and menu item.

--- Java/MVC_example/src/com/guiwork/View.java
+++ Java/MVC_example/src/com/guiwork/View.java
@@ -5,6 +5,9 @@ import oracle.jvm.hotspot.jfr.JFR;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
 
 
 /**
@@ -12,16 +15,33 @@ import java.awt.BorderLayout;
  */
 public class View {
     private JFrame frame;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem menuItem0;
     private JLabel label;
     private JButton buttonInc, buttonDec, buttonReset;
 
     public View(String text) {
         frame = new JFrame("View");
         frame.getContentPane().setLayout(new BorderLayout());
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(200, 200);
         frame.setVisible(true);
+
+        //Build the first menu
+        menuBar = new JMenuBar();
+        menu = new JMenu("A Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription("Here is a placeholder.");
+        menuBar.add(menu);
+
+        //A group of JMenuItems
+        menuItem0 = new JMenuItem("A text-only menu item", KeyEvent.VK_T);
+        menuItem0.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem0.getAccessibleContext().setAccessibleDescription("Doesn't do anything, yet.");
+        menu.add(menuItem0);
+
+        frame.setJMenuBar(menuBar);
 
         label = new JLabel(text);
         frame.getContentPane().add(label, BorderLayout.CENTER);
